71cae08f9ccf26317d088c111e1d0300df902f6e
Stefan Ouweneel <stefanouweneel@gmail.com>
2021-04-23 09:04:44 +0200

    individual stat manipulation and equipment + companion slide functionality

--- src/Compbox.js
+++ src/Compbox.js
@@ -1,20 +1,48 @@
-import lahksa from './images/lahksa.gif';
+import React, {useState, useEffect} from 'react';
+import './Compbox.css';
 
-function Compbox() {
+export default function Compselect({images=[]}){
+  const [currentImage, setImage] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [currentSlideStyle, setCurrentSlideStyle] = useState({});
+
+  useEffect(()=>{
+    setImage(images);
+    setCurrentSlideStyle({
+      backgroundImage: "url('"+images[currentSlide]+"')"
+    });
+  }, [images, currentSlide]);
+
+  function previous(){
+    if (currentSlide > 0){
+      setCurrentSlide(currentSlide-1);
+    }
+    else {
+      setCurrentSlide(images.length-1);
+    }
+  }
+
+  function next(){
+    if (currentSlide === images.length-1){
+      setCurrentSlide(0);
+    }
+    else {
+      setCurrentSlide(currentSlide+1);
+    }
+  }
+
   return (
-    <div>
-      <h4>Select your companion</h4>
-      <img src={lahksa} width="18%" />
-      <p>
-        <button>
-          &lt;
-        </button>
-        <button>
-          &gt;
-        </button>
-      </p>
+  <div>
+    <h4>Select your companion</h4>
+    <div style={currentSlideStyle} className="comp-slide-holder" />
+    <div className="slideshow-controller">
+      <button onClick={previous}>
+        &lt;
+      </button>
+      <button onClick={next}>
+        &gt;
+      </button>
     </div>
-  );
+  </div>
+  )
 }
-
-export default Compbox;
